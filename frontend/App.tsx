0542f6c821d9b375f94ca8fac6f38ba15954c288
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-17 20:13:43 +0900

    chore: react navigation 설정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,54 @@
-import React from "react";
-import { Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { Text, SafeAreaView, StyleSheet, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+
+  const handleChangeInput = (text: string) => {
+    setName(text);
+  };
   return (
-    <SafeAreaView>
-      <Text>app 초기화</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{name}</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text>문자</Text>
+        <TextInput
+          value={name}
+          style={styles.input}
+          onChangeText={handleChangeInput}
+        />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginHorizontal: 10,
+  },
+  input: {
+    height: 50,
+    width: 100,
+    flex: 1,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+});

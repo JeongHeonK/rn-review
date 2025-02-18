ce4e5dcfb18c8b17ab3a2144d6535c9cdab507f0
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-18 15:10:44 +0900

    feat: Home 화면 수정

--- frontend/src/screens/auth/AuthHomeScreen.tsx
+++ frontend/src/screens/auth/AuthHomeScreen.tsx
@@ -1,6 +1,12 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from "react-native";
 import { AuthStackParamList } from "../../navigations/stack/AuthStackNavigator";
 import { authNavigations } from "../../constants";
 import CustomButton from "../../components/CustomButton";
@@ -12,26 +18,48 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({ navigation }: AuthHomeScreenProps) {
   return (
-    <View style={styles.container}>
-      <CustomButton
-        label="로그인 화면으로 이동"
-        onPress={() => navigation.navigate(authNavigations.LOGIN)}
-      />
-      <CustomButton
-        variant="outlined"
-        label="회원가입 화면으로 이동"
-        onPress={() => navigation.navigate(authNavigations.SIGNUP)}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require("../../assets/practice.jpg")}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          label="로그인 화면으로 이동"
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
+        />
+        <CustomButton
+          variant="outlined"
+          label="회원가입 화면으로 이동"
+          onPress={() => navigation.navigate(authNavigations.SIGNUP)}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: 12,
-    justifyContent: "center",
+    margin: 30,
     alignItems: "center",
+  },
+  imageContainer: {
+    flex: 2,
+    alignItems: "center",
+    width: Dimensions.get("screen").width / 1.5,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+  },
+  buttonContainer: {
+    gap: 12,
+    flex: 1,
   },
 });
 

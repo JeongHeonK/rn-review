b4d4845772e963ce5ba86cd5efb2d31709e60fa7
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-18 14:14:50 +0900

    refactor: CustomButton

--- frontend/src/components/CustomButton.tsx
+++ frontend/src/components/CustomButton.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import {
+  Dimensions,
+  Pressable,
+  PressableProps,
+  StyleSheet,
+  Text,
+} from "react-native";
+import { colors } from "../constants";
 
-interface CustomButtonProps {
+interface CustomButtonProps extends PressableProps {
   label: string;
-  variant: "filled" | "outlined";
+  variant?: "filled" | "outlined";
+  size?: "large" | "medium";
+  invalid?: boolean;
   onPress?: () => void;
 }
 
-function CustomButton({ label, variant, onPress }: CustomButtonProps) {
+const deviceHeight = Dimensions.get("screen").height;
+
+function CustomButton({
+  label,
+  variant = "filled",
+  size = "large",
+  invalid = false,
+  ...props
+}: CustomButtonProps) {
   return (
-    <Pressable style={[styles.container, styles[variant]]} onPress={onPress}>
-      <Text style={styles.text}>{label}</Text>
+    <Pressable
+      disabled={invalid}
+      style={[
+        styles.container,
+        styles[variant],
+        styles[size],
+        invalid && styles.invalid,
+      ]}
+      {...props}
+    >
+      <Text style={[styles[`${variant}Text`], styles[`${size}Text`]]}>
+        {label}
+      </Text>
     </Pressable>
   );
 }
@@ -19,17 +47,40 @@ const styles = StyleSheet.create({
   container: {
     borderRadius: 8,
     justifyContent: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+  },
+  invalid: {
+    opacity: 0.5,
   },
   filled: {
-    backgroundColor: "#a1e3f9",
+    backgroundColor: colors.BLUE_800,
   },
   outlined: {
-    borderColor: "#3674b5",
+    borderColor: colors.BLUE_800,
     borderWidth: 1,
   },
-  text: {
+  large: {
+    width: "90%",
+    paddingVertical: deviceHeight > 700 ? 15 : 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  medium: {
+    width: "50%",
+    paddingVertical: deviceHeight > 700 ? 12 : 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  filledText: {
+    color: colors.WHITE,
+  },
+  outlinedText: {
+    color: colors.BLACK,
+  },
+  largeText: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  mediumText: {
     fontSize: 14,
   },
 });
